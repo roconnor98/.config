fcccfe112bef1ba285df63f4c81e2c8fbee35568
Ruairi <ruairi@megit.com>
2026-03-09 12:24:05 +0000

    Adding telescope

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,4 +1,9 @@
 -- Leader key
 vim.g.mapleader = ' '
+
 -- To back out to file view
 vim.keymap.set('n', '-', vim.cmd.Ex)
+
+-- Telescope find files
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>=', builtin.find_files, { desc = 'Telescope find files' })
