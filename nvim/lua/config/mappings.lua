3cba1104072e77c483a47bd2727a6cce1f220d0a
Ruairí O'Connor <ruairioconnor98@gmail.com>
2026-03-19 17:32:45 +0000

    Adding Harpoon

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -10,3 +10,16 @@ vim.keymap.set('n', '<leader>g', "<cmd>lua require'telescope.builtin'.live_grep(
 
 -- Diagnostics
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show line diagnostics' })
+
+-- Harpoon
+vim.keymap.set("n", "<leader>a", function() require('harpoon'):list():add() end)
+vim.keymap.set("n", "<C-e>", function() require('harpoon').ui:toggle_quick_menu(require('harpoon'):list()) end)
+
+vim.keymap.set("n", "<C-h>", function() require('harpoon'):list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() require('harpoon'):list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() require('harpoon'):list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() require('harpoon'):list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() require('harpoon'):list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() require('harpoon'):list():next() end)
